237193971898077a04568f29dda230dd952ac1da
18631323023 <qq492690314>
2019-05-07 10:56:21 +0800

    意见反馈实体类

---
+++ src/main/java/com/wisewin/backend/entity/dto/FeedbackDTO.java
@@ -0,0 +1,99 @@
+package com.wisewin.backend.entity.dto;
+
+
+
+
+import java.util.Date;
+
+public class FeedbackDTO {
+    private Integer id;//意见反馈
+    private Integer userId;//用户id
+    private String content;//反馈内容
+    private String image;//图片
+    private String contactWay;//联系方式
+    private String contactNumber;//联系号码
+    private Date createTime;//创建时间
+    private String status;//状态
+    private Integer adminId;//操作员id
+    private Date updateTime;//修改时间
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getContactWay() {
+        return contactWay;
+    }
+
+    public void setContactWay(String contactWay) {
+        this.contactWay = contactWay;
+    }
+
+    public String getContactNumber() {
+        return contactNumber;
+    }
+
+    public void setContactNumber(String contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Integer getAdminId() {
+        return adminId;
+    }
+
+    public void setAdminId(Integer adminId) {
+        this.adminId = adminId;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+}
