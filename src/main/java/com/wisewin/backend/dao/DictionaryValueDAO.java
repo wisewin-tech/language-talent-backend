9ef834fc9dd21ca44d20d69cd4b2506ffd8f9bb6
zccxywy <18731612878@163.com>
2019-06-14 21:03:58 +0800

    bug

--- src/main/java/com/wisewin/backend/dao/DictionaryValueDAO.java
+++ src/main/java/com/wisewin/backend/dao/DictionaryValueDAO.java
@@ -2,6 +2,7 @@ package com.wisewin.backend.dao;
 
 import com.wisewin.backend.entity.bo.DictionaryBO;
 import com.wisewin.backend.entity.bo.DictionaryValueBO;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
@@ -11,7 +12,7 @@ public interface DictionaryValueDAO {
     //增
     Integer addDic(DictionaryValueBO dictionaryBO);
     //删
-    Integer delDic(Integer[] idArr);
+    Integer delDic(@Param("idArr")Integer[] idArr);
 
     //改
     Integer updDic(DictionaryValueBO dictionaryBO);
