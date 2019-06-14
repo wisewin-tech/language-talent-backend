2010eef37846ad44041f1d2d9e30fd7cecc40605
wangbin <1533164866@qq.com>
2019-06-14 15:11:47 +0800

    修改bug

--- src/main/java/com/wisewin/backend/util/redisUtils/RedissonHandler.java
+++ src/main/java/com/wisewin/backend/util/redisUtils/RedissonHandler.java
@@ -20,7 +20,7 @@ public class RedissonHandler {
 
     private final String port = "6379";
 
-    private final String ip ="localhost";//"localhost";// "r-2zec622a372f0b04.redis.rds.aliyuncs.com";//;
+    private final String ip ="r-2zec622a372f0b04.redis.rds.aliyuncs.com";//"localhost";// "r-2zec622a372f0b04.redis.rds.aliyuncs.com";//;
 
     private final String password = null;
 
